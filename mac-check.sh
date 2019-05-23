57614d5c8a9047b7eb69a24f4c8a6213bbd37c45
Mao <lcdsmao@gmail.com>
2019-05-24 00:20:26 +0900

    Remove redundant brew bundle

--- mac-check.sh
+++ mac-check.sh
@@ -5,7 +5,6 @@ if !(type brew > /dev/null 2>&1); then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew bundle
 fi
-brew bundle
 
 if [[ ! -e ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
