6df70ca6e86f6276357bbcdd230ec1a5b4245446
Mao Yufeng <lcdsmao@gmail.com>
2019-11-27 11:10:41 +0900

    Fzf use fd as default

--- oh-my-zsh/custom/fzf.zsh
+++ oh-my-zsh/custom/fzf.zsh
@@ -12,6 +12,25 @@ fi
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
+# Setting fd as the default source for fzf
+export FZF_DEFAULT_COMMAND='fd --type f'
+
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
 # ------------
 # Begin custom functions
 
