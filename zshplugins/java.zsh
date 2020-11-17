30264df060debbdd75c5b753c3aba20ee2737b2a
Mao <lcdsmao@gmail.com>
2020-11-17 10:30:32 +0900

    java_home wrokaround

--- zshplugins/java.zsh
+++ zshplugins/java.zsh
@@ -1,8 +1,10 @@
 jdk() {
     version=$1
+    # Big Sur bug: https://developer.apple.com/forums/thread/666681
+    unset JAVA_HOME
     export JAVA_HOME=$(/usr/libexec/java_home -v"$version")
     java -version
 }
 
 # Set default JAVA_HOME
-jdk 11 > /dev/null 2>&1
+# jdk 11 > /dev/null 2>&1
