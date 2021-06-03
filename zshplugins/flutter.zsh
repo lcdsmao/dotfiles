c76332bc8d49c54c694f8ac0fb2c0295f59bb958
Mao <lcdsmao@gmail.com>
2021-06-03 14:53:05 +0900

    feat: hot restart

--- zshplugins/flutter.zsh
+++ zshplugins/flutter.zsh
@@ -2,6 +2,10 @@ fhr() {
   local snapshot
   snapshot="$(pgrep -f flutter_tools.snapshot\ run)"
   if [[ -n $snapshot ]]; then
-    kill -SIGUSR1 "$snapshot" &> /dev/null
+    if [[ $1 == '-f' ]]; then
+      kill -SIGUSR2 "$snapshot" &> /dev/null
+    else
+      kill -SIGUSR1 "$snapshot" &> /dev/null
+    fi
   fi
 }
