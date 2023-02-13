ee186c34f0bf0ec5c9ecb9f3dc8bf0585e947ac8
Mao Yufeng <lcdsmao@gmail.com>
2023-02-13 17:59:57 +0900

    feat: update brew install

--- os-check.sh
+++ os-check.sh
@@ -2,7 +2,7 @@
 mac_requirement() {
   xcode-select --install
   if ! (type brew > /dev/null 2>&1); then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew bundle
   else
     echo brew already installed
