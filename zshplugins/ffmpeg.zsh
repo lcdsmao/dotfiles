4c6a3f78a678ce0919b6fe74892555f3a5cab7a7
lcdsmao <lcdsmao@gmail.com>
2023-09-01 10:03:29 +0900

    feat: ffresize support pass argument

--- zshplugins/ffmpeg.zsh
+++ zshplugins/ffmpeg.zsh
@@ -6,7 +6,7 @@ function ffresize() {
   output_dir=$(dirname "$input_file")
 
   # Use fzf to prompt user to choose scaling option
-  scale=$(echo "1920:-1, 1280:-1, 720:-1, 480:-1, 360:-1" | tr ',' '\n' | fzf)
+  scale="${2:-"$(echo "1920:-1, 1280:-1, 720:-1, 480:-1, 360:-1" | tr ',' '\n' | fzf)"}"
 
   # Use ffmpeg to resize video
   ffmpeg -i "$input_file" -vf scale="$scale" "$output_dir/${input_file_name}_2.${input_file_extension}"
