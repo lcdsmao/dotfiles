5d2156c6c2fe134c235bfd16a54e1211872ab289
Mao <lcdsmao@gmail.com>
2024-07-27 11:08:15 +0900

    feat: function to delete squashed branches

--- zshplugins/git.zsh
+++ zshplugins/git.zsh
@@ -1,14 +1,16 @@
-gitD2w() {
-  for k in $(git branch --format="%(refname:short)"); do
-    if (($(git log -1 --since='2 week ago' -s "$k" | wc -l) == 0)); then
-      echo git branch -D "$k"
-    fi
-  done
-}
-
 gitdb() {
-  local main_branch
   main_branch=$(git_main_branch)
   git branch --merged="$main_branch" | grep -v "$main_branch" | xargs git branch -d
   git fetch --prune
+}
+
+gitdsb() {
+  main_branch=$(git_main_branch)
+  branches=$(git checkout -q "$main_branch" && git for-each-ref refs/heads/ "--format=%(refname:short)")
+  while read branch; do
+    mergeBase=$(git merge-base "$main_branch" "$branch")
+    if [[ $(git cherry "$main_branch" "$(git commit-tree "$(git rev-parse "$branch^{tree}")" -p "$mergeBase" -m _)") == "-"* ]]; then
+      echo git branch -D "$branch"
+    fi
+  done <<< "$branches"
 }
