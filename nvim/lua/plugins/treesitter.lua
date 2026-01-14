b12d7d66056260b41d6011d9dcb60a7d723d0a9f
lcdsmao <lcdsmao@gmail.com>
2026-01-14 15:42:43 +0900

    fix: treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,15 +3,40 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
-    config = function(_, opts)
-      local treesitter = require("nvim-treesitter")
-      treesitter.setup(opts)
-      local installed = treesitter.get_installed()
-      vim.api.nvim_create_autocmd("FileType", {
-        -- Use the list of installed parsers as the pattern
-        pattern = installed,
+    config = function()
+      -- Install parsers
+      require('nvim-treesitter').install({
+        "markdown",
+        "markdown_inline",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "typescript",
+        "python",
+        "rust",
+        "go",
+        "java",
+        "c",
+        "cpp",
+        "bash",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "diff",
+      })
+
+      -- Enable treesitter features for all installed parsers
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*',
         callback = function()
-          vim.treesitter.start()
+          local ok, _ = pcall(vim.treesitter.start)
+          if ok then
+            -- Enable treesitter-based indentation
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
         end,
       })
     end,
