bfa58deb8bcb8a97ab3731e569bd3ff20dbcca1a
lcdsmao <lcdsmao@gmail.com>
2026-01-20 19:56:27 +0900

    feat: update fzf config

--- zshplugins/android.zsh
+++ zshplugins/android.zsh
@@ -18,10 +18,10 @@ function lctag() { lcg "$1":V '*:S'; }
 function lcapp() { lcg --pid="$(adb shell pidof -s "$(adbsp)")"; }
 
 function adbanim() {
-  local factor=${1:-1}
-  adb shell settings put global window_animation_scale "$factor"
-  adb shell settings put global transition_animation_scale "$factor"
-  adb shell settings put global animator_duration_scale "$factor"
+    local factor=${1:-1}
+    adb shell settings put global window_animation_scale "$factor"
+    adb shell settings put global transition_animation_scale "$factor"
+    adb shell settings put global animator_duration_scale "$factor"
 }
 
 alias adbgproxy='adb shell settings get global http_proxy;'
@@ -30,122 +30,144 @@ alias adbrproxy='adb shell settings put global http_proxy :0'
 alias adbeam='adb shell cmd connectivity airplane-mode enable'
 alias adbdam='adb shell cmd connectivity airplane-mode disable'
 
+function avdr() {
+    selected_avd="$(emulator -list-avds | fzf)" && (emulator @"$selected_avd" "$@" > /dev/null 2>&1 &)
+}
+
+function adb_select_device() {
+    local devs
+    local selected_dev
+    local dev_cnt
+    devs="$(command adb devices -l | awk 'NR > 1 { print }')"
+    dev_cnt="$(echo "$devs" | wc -l)"
+    if [[ $dev_cnt -le 1 ]]; then
+        selected_dev="$devs"
+    else
+        selected_dev="$(echo "$devs" | fzf)"
+    fi
+    echo "$selected_dev" | awk '{ print $1 }'
+}
+
 function adbt() {
-  case "$1" in
-    -a)
-      export _adb_type="ALL"
-      echo "adb type: ALL"
-      ;;
-    -f)
-      export _adb_type="FZF"
-      echo "adb type: FZF"
-      ;;
-    -d)
-      unset _adb_type
-      echo "adb type: DEFAULT"
-      ;;
-    *)
-      export _adb_type="ONE"
-      echo "adb type: ONE"
-      ;;
-  esac
+    case "$1" in
+        -a)
+            export _adb_type="ALL"
+            echo "adb type: ALL"
+            ;;
+        -f)
+            export _adb_type="FZF"
+            echo "adb type: FZF"
+            ;;
+        -d)
+            unset _adb_type
+            echo "adb type: DEFAULT"
+            ;;
+        *)
+            export _adb_type="ONE"
+            echo "adb type: ONE"
+            ;;
+    esac
 }
 
 function adb() {
-  case "$_adb_type" in
-    "ALL")
-      adb_all "$@"
-      ;;
-    "FZF")
-      adb_fzf "$@"
-      ;;
-    "ONE")
-      adb_one "$@"
-      ;;
-    *)
-      command adb "$@"
-      ;;
+    case "$_adb_type" in
+        "ALL")
+            adb_all "$@"
+            ;;
+        "FZF")
+            adb_fzf "$@"
+            ;;
+        "ONE")
+            adb_one "$@"
+            ;;
+        *)
+            command adb "$@"
+            ;;
 
-  esac
+    esac
 }
 
 function adb_all() {
-  local ds=()
-  while IFS='' read -r line; do ds+=("$line"); done < <(command adb devices | awk 'NR > 1 {print $1 }')
-  for i in "${ds[@]}"; do
-    [[ -n $i ]] && command adb -s "$i" "$@"
-  done
+    local ds=()
+    while IFS='' read -r line; do ds+=("$line"); done < <(command adb devices | awk 'NR > 1 {print $1 }')
+    for i in "${ds[@]}"; do
+        [[ -n $i ]] && command adb -s "$i" "$@"
+    done
+}
+
+function adb_fzf() {
+    command adb -s "$(adb_select_device)" "$@"
 }
 
 function adb_one() {
-  local ds=()
-  while IFS='' read -r line; do ds+=("$line"); done < <(command adb devices | awk 'NR > 1')
-  for i in "${ds[@]}"; do
-    read -r device state <<< "$i"
-    if [[ $state = "device" ]]; then
-      command adb -s "$device" "$@"
-      break
-    fi
-  done
+    local ds=()
+    while IFS='' read -r line; do ds+=("$line"); done < <(command adb devices | awk 'NR > 1')
+    for i in "${ds[@]}"; do
+        read -r device state <<< "$i"
+        if [[ $state = "device" ]]; then
+            command adb -s "$device" "$@"
+            break
+        fi
+    done
 }
 
 function adbshot() {
-  DATE=$(date '+%y%m%d%H%M%S')
-  FILE_NAME=screenshot-${DATE}.png
-  DIR_PATH=~/Desktop
+    DATE=$(date '+%y%m%d%H%M%S')
+    FILE_NAME=screenshot-${DATE}.png
+    DIR_PATH=~/Desktop
 
-  local selected_dev
-  selected_dev=$(adb_select_device)
-  command adb -s "$selected_dev" shell screencap -p > "${DIR_PATH}/${FILE_NAME}"
+    local selected_dev
+    selected_dev=$(adb_select_device)
+    command adb -s "$selected_dev" shell screencap -p > "${DIR_PATH}/${FILE_NAME}"
 }
 
 function adbrecord() {
-  DATE=$(date '+%y%m%d%H%M%S')
-  FILE_NAME=record-${DATE}
-  YOUR_PATH=~/Desktop
+    DATE=$(date '+%y%m%d%H%M%S')
+    FILE_NAME=record-${DATE}
+    YOUR_PATH=~/Desktop
 
-  local selected_dev
-  selected_dev=$(adb_select_device)
+    local selected_dev
+    selected_dev=$(adb_select_device)
 
-  command adb -s "$selected_dev" shell screenrecord /sdcard/"$FILE_NAME".mp4 &
-  pid=$(ps x | grep -v grep | grep "shell screenrecord" | awk '{ print $1 }')
+    command adb -s "$selected_dev" shell screenrecord /sdcard/"$FILE_NAME".mp4 &
+    pid=$(ps x | grep -v grep | grep "shell screenrecord" | awk '{ print $1 }')
 
-  if [ -z "$pid" ]; then
-    printf "Not running a screenrecord."
-    return 1
-  fi
-
-  printf "Recording, finish? [y]"
-  while read -r isFinished; do
-    case "$isFinished" in
-      "y" | "Y") break ;;
-      *) printf "Incorrect value." ;;
-    esac
-  done
-
-  kill -9 "$pid" # Finished the process of adb screenrecord
-  while :; do
-    alive=$(command adb -s "$selected_dev" shell ps | grep screenrecord | grep -v grep | awk '{ print $9 }')
-    if [ -z "$alive" ]; then
-      break
+    if [ -z "$pid" ]; then
+        printf "Not running a screenrecord."
+        return 1
     fi
-  done
 
-  printf "Finished the recording process : %s\nSending to %s...\n" "$pid" "$YOUR_PATH"
-  command adb -s "$selected_dev" pull /sdcard/"${FILE_NAME}".mp4 $YOUR_PATH
-  command adb -s "$selected_dev" shell rm /sdcard/"${FILE_NAME}".mp4
+    printf "Recording, finish? [y]"
+    while read -r isFinished; do
+        case "$isFinished" in
+            "y" | "Y") break ;;
+            *) printf "Incorrect value." ;;
+        esac
+    done
 
-  echo "Converts to GIF? [y]"
-  read -r convertGif
-  case $convertGif in
-    "y" | "Y") ffmpeg -i "${YOUR_PATH}/${FILE_NAME}.mp4" -an -r 15 -pix_fmt rgb24 -f gif "${YOUR_PATH}/${FILE_NAME}.gif" ;; # creating gif
-    *) ;;
-  esac
+    kill -9 "$pid" # Finished the process of adb screenrecord
+    while :; do
+        alive=$(command adb -s "$selected_dev" shell ps | grep screenrecord | grep -v grep | awk '{ print $9 }')
+        if [ -z "$alive" ]; then
+            break
+        fi
+    done
+
+    printf "Finished the recording process : %s\nSending to %s...\n" "$pid" "$YOUR_PATH"
+    command adb -s "$selected_dev" pull /sdcard/"${FILE_NAME}".mp4 $YOUR_PATH
+    command adb -s "$selected_dev" shell rm /sdcard/"${FILE_NAME}".mp4
+
+    echo "Converts to GIF? [y]"
+    read -r convertGif
+    case $convertGif in
+        "y" | "Y") ffmpeg -i "${YOUR_PATH}/${FILE_NAME}.mp4" -an -r 15 -pix_fmt rgb24 -f gif "${YOUR_PATH}/${FILE_NAME}.gif" ;; # creating gif
+        *) ;;
+    esac
 }
 
 function adbwifi() {
-  adb tcpip 5555 \
-    && sleep 1 \
-    && ip=$(adb shell "ip addr show wlan0 | grep -e wlan0$ | cut -d\" \" -f 6 | cut -d/ -f 1") \
-    && adb connect "$ip":5555
+    adb tcpip 5555 \
+        && sleep 1 \
+        && ip=$(adb shell "ip addr show wlan0 | grep -e wlan0$ | cut -d\" \" -f 6 | cut -d/ -f 1") \
+        && adb connect "$ip":5555
 }
--- zshplugins/fzf.zsh
+++ zshplugins/fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
 # To install useful key bindings and fuzzy completion:
@@ -17,7 +17,8 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Default Options
 export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history
---bind ctrl-a:select-all,ctrl-d:deselect-all"
+--bind ctrl-a:select-all,ctrl-d:deselect-all
+--bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down"
 # Dogrun theme
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
   --color=fg:#8085a6,fg+:#d0d0d0,bg:-1,bg+:#262626
@@ -32,186 +33,99 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
 _fzf_compgen_path() {
-  fd --hidden --follow --exclude ".git" . "$1"
+    fd --hidden --follow --exclude ".git" . "$1"
 }
 
 # Use fd to generate the list for directory completion
 _fzf_compgen_dir() {
-  fd --type d --hidden --follow --exclude ".git" . "$1"
+    fd --type d --hidden --follow --exclude ".git" . "$1"
 }
 
 # ------------
 # Begin custom functions
 
 ###################################################
-# Preview
-function fp() {
-  fzf --preview '[[ $(file --mime {}) =~ binary ]] \
-    && echo {} is a binary file \
-    || (bat --style=numbers --color=always {} \
-    || highlight -O ansi -l {} \
-    || coderay {} \
-    || rougify {} \
-    || cat {}) 2> /dev/null | head -500'
-}
-
-###################################################
 # Change directory
 # fdr - cd to selected parent directory
 function fdr() {
-  local dirs=()
-  get_parent_dirs() {
-    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
-    if [[ "${1}" == '/' ]]; then
-      for _dir in "${dirs[@]}"; do echo "$_dir"; done
-    else
-      get_parent_dirs "$(dirname "$1")"
-    fi
-  }
-  local DIR
-  DIR=$(get_parent_dirs "$(realpath "${1:-$PWD}")" | fzf-tmux --tac)
-  cd "$DIR" || return
+    local dirs=()
+    get_parent_dirs() {
+        if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
+        if [[ "${1}" == '/' ]]; then
+            for _dir in "${dirs[@]}"; do echo "$_dir"; done
+        else
+            get_parent_dirs "$(dirname "$1")"
+        fi
+    }
+    local DIR
+    DIR=$(get_parent_dirs "$(realpath "${1:-$PWD}")" | fzf-tmux --tac)
+    cd "$DIR" || return
 }
 
 ###################################################
-# fd - cd to selected directory
-# function fd() {
-#     local dir
-#     dir=$(find ${1:-.} -path '*/\.*' -prune \
-#         -o -type d -print 2> /dev/null | fzf +m) &&
-#         cd "$dir"
-#     }
-
 # cdf - cd into the directory of the selected file
 function cdf() {
-  local file
-  local dir
-  file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir" || return
+    local file
+    local dir
+    file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir" || return
 }
 
 ###################################################
 # Git
-# fbr - checkout git branch (including remote branches)
-function fbr() {
-  local branches branch
-  branches=$(git branch --all | grep -v HEAD) \
-    && branch=$(echo "$branches" | fzf-tmux -d $((2 + $(wc -l <<< "$branches"))) +m) \
-    && git checkout "$(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")"
+# fco_preview - checkout git branch/tag, with a preview showing the commits between the tag/branch and HEAD
+function fco() {
+    local tags branches target
+    branches=$(
+        git --no-pager branch --all \
+            --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" \
+            | sed '/^$/d'
+    ) || return
+    tags=$(
+        git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}'
+    ) || return
+    target=$(
+        (
+            echo "$branches"
+            echo "$tags"
+        ) \
+            | fzf --no-hscroll --no-multi -n 2 \
+                --ansi --preview="git --no-pager log -150 --pretty=format:%s '..{2}'"
+    ) || return
+    git checkout $(awk '{print $2}' <<< "$target")
 }
 
-# fga - view status
-function fga() {
-  modified_files=$(git status --short | awk '{print $2}') \
-    && selected_files=$(echo "$modified_files" \
-      | fzf -m --preview 'git diff head {} | (bat --style=numbers --color=always || cat {}) 2> /dev/null') \
-    && git add "$selected_files"
+# fshow - git commit browser
+function fshow() {
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" \
+        | fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+            --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
 }
 
-# fga - view status
-function fgd() {
-  modified_files=$(git diff "$1" --name-only) \
-    && echo "$modified_files" \
-    | fzf -m --preview "git diff $1 {} \
-    | (bat --style=numbers --color=always || cat {}) 2> /dev/null"
-}
-
-###################################################
-# Homebrew
-# Install (one or multiple) selected application(s)
-# using "brew search" as source input
-# mnemonic [B]rew [I]nstall [P]lugin
-function bip() {
-  local inst
-  inst=$(brew search | fzf -m)
-
-  if [[ $inst ]]; then
-    for prog in $(echo $inst); do
-      brew install "$prog"
-    done
-  fi
-}
-
-# Update (one or multiple) selected application(s)
-# mnemonic [B]rew [U]pdate [P]lugin
-function bup() {
-  local upd
-  upd=$(brew leaves | fzf -m)
-
-  if [[ $upd ]]; then
-    for prog in $(echo $upd); do
-      brew upgrade $prog
-    done
-  fi
-}
-
-# Delete (one or multiple) selected application(s)
-# mnemonic [B]rew [C]lean [P]lugin (e.g. uninstall)
-function bcp() {
-  local uninst
-  uninst=$(brew leaves | fzf -m)
-
-  if [[ $uninst ]]; then
-    for prog in $(echo $uninst); do
-      brew uninstall "$prog"
-    done
-  fi
-}
-
-###################################################
-# Interactive cd
-# Suggested by @mgild Like normal cd but opens an interactive navigation window when called with no arguments. For ls, use -FG instead of --color=always on osx.
-function cd() {
-  if [[ "$#" != 0 ]]; then
-    builtin cd "$@"
-    return
-  fi
-  while true; do
-    local lsd=$(echo ".." && /bin/ls -p | grep '/$' | sed 's;/$;;')
-    local dir="$(printf '%s\n' "${lsd[@]}" \
-      | fzf --reverse --preview ' \
-        __cd_nxt="$(echo {})"; \
-        __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")"; \
-        echo $__cd_path; \
-        echo; \
-        /bin/ls -pG "${__cd_path}"; \
-        ')"
-    [[ ${#dir} != 0 ]] || return 0
-    builtin cd "$dir" &> /dev/null
-  done
+# fcs - get git commit sha
+# example usage: git rebase -i `fcs`
+function fcs() {
+    local commits commit
+    commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse "${1:-HEAD}") \
+        && commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) \
+        && echo -n "$(echo "$commit" | sed "s/ .*//")"
 }
 
 ###################################################
 # Autojump
 function j() {
-  if [[ "$#" -ne 0 ]]; then
-    cd "$(autojump $@)"
-    return
-  fi
-  cd "$(autojump -s | sed -e :a -e '$d;N;2,7ba' -e 'P;D' | awk '{print $2}' | fzf --height 40% --reverse --inline-info)"
-}
+    local preview_cmd="ls -la {}"
+    if command -v eza &> /dev/null; then
+        preview_cmd="eza -la --color=always {}"
+    fi
 
-###################################################
-# Android emulator
-function avdr() {
-  selected_avd="$(emulator -list-avds | fzf)" \
-    && (emulator @"$selected_avd" "$@" > /dev/null 2>&1 &)
-}
-
-function adb_select_device() {
-  local devs
-  local selected_dev
-  local dev_cnt
-  devs="$(command adb devices -l | awk 'NR > 1 { print }')"
-  dev_cnt="$(echo "$devs" | wc -l)"
-  if [[ $dev_cnt -le 1 ]]; then
-    selected_dev="$devs"
-  else
-    selected_dev="$(echo "$devs" | fzf)"
-  fi
-  echo "$selected_dev" | awk '{ print $1 }'
-}
-
-# adb
-function adb_fzf() {
-  command adb -s "$(adb_select_device)" "$@"
+    if [[ $# -eq 0 ]]; then
+        cd "$(autojump -s | grep -E '^\s*[0-9]+\.[0-9]+:\s+/' | sort -t: -k1 -rn | awk -F'\t' '{print $2}' | fzf --height 60% --reverse --inline-info --no-sort --tiebreak=index --preview "$preview_cmd")" || return
+    else
+        cd "$(autojump "$@")" || return
+    fi
 }
