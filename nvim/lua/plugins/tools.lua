a706f19729b84a6a91cc30d0c5ad52df778a9cbd
lcdsmao <lcdsmao@gmail.com>
2026-01-21 15:46:39 +0900

    feat: remove floaterm in favor of popup tmux

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -1,19 +1,5 @@
 return {
   {
-    "voldikss/vim-floaterm",
-    lazy = false,
-    init = function()
-      -- These need to be set BEFORE the plugin loads
-      vim.g.floaterm_width = 0.9
-      vim.g.floaterm_height = 0.9
-      vim.g.floaterm_keymap_toggle = '<C-Space><C-Space>'
-      vim.g.floaterm_borderchars = '─│─│╭╮╯╰]'
-    end,
-    config = function()
-      vim.cmd([[hi FloatermBorder guibg=#222433]])
-    end,
-  },
-  {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npx --yes yarn install",
     ft = "markdown",
