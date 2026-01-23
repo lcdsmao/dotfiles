ef443bf3ec41f946951fab6f65497ee5c1e49fd0
lcdsmao <lcdsmao@gmail.com>
2026-01-23 10:36:50 +0900

    feat: improve j with tmux

--- zshplugins/zoxide.zsh
+++ zshplugins/zoxide.zsh
@@ -1,11 +1,55 @@
-if (( $+commands[zoxide] )); then
-    eval "$(zoxide init --cmd ${ZOXIDE_CMD_OVERRIDE:-z} zsh)"
-
-    function j() {
-        if [[ $# -eq 0 ]]; then
-            zi
-        else
-            z "$@"
-        fi
-    }
+if ! command -v zoxide > /dev/null 2>&1; then
+    return
 fi
+
+eval "$(zoxide init --cmd ${ZOXIDE_CMD_OVERRIDE:-z} zsh)"
+
+# Jump to the most recently used directory, with tmux integration
+# If already in a tmux window with that directory, prompt to switch to it
+function j() {
+    if [[ $# -ne 0 ]]; then
+        z "$@"
+        return
+    fi
+
+    local target_path
+    target_path="$(zoxide query -i 2> /dev/null)" || return
+    if [[ -z "$target_path" ]]; then
+        return
+    fi
+
+    target_path="$(cd "$target_path" && pwd -P)" || return
+
+    if [[ -n "$TMUX" ]]; then
+        local current_window
+        current_window="$(tmux display-message -p '#{window_id}')"
+        local target_window=""
+        while read -r window_id; do
+            local window_path=""
+            while read -r pane_path; do
+                window_path="$pane_path"
+                break
+            done < <(tmux list-panes -t "$window_id" -F '#{pane_current_path}')
+
+            if [[ "$window_path" == "$target_path" ]]; then
+                target_window="$window_id"
+                break
+            fi
+        done < <(tmux list-windows -F '#{window_id}')
+
+        if [[ -n "$target_window" && "$target_window" == "$current_window" ]]; then
+            return
+        fi
+
+        if [[ -n "$target_window" ]]; then
+            local reply
+            read -r "reply?Jump to window already at $target_path? [y/N] "
+            if [[ "$reply" == [yY]* ]]; then
+                tmux select-window -t "$target_window"
+                return
+            fi
+        fi
+    fi
+
+    cd "$target_path" || return
+}
