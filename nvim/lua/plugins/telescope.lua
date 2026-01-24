ab3ae45da2d9b14dac49a956dac3315923b00fc5
Mao <lcdsmao@gmail.com>
2026-01-24 11:11:36 +0900

    feat: add dynamic rg filter to live_grep with <C-f> keymap
    Adds a <C-f> keymap in live_grep that opens an input prompt to add
    rg parameters without losing the current search query. Parameters are
    parsed and passed as additional_args to ripgrep. Users can press <C-f>
    then <CR> with empty input to clear filters.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,12 +6,12 @@ return {
       { "nvim-telescope/telescope-fzf-native.nvim", build = 'make' },
     },
     keys = {
-      { "<leader>ft", "<cmd>Telescope<cr>", desc = "Telescope" },
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
+      { "<leader>ft", "<cmd>Telescope<cr>",                                       desc = "Telescope" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>",                            desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>",                             desc = "Find git files" },
       { "<leader>fa", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", desc = "Find all files" },
-      { "<leader>fr", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+      { "<leader>fr", "<cmd>Telescope live_grep<cr>",                             desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",                               desc = "Find buffers" },
     },
     opts = {
       defaults = {
@@ -29,6 +29,44 @@ return {
             ["<C-q>"] = require('telescope.actions').smart_send_to_qflist + require('telescope.actions').open_qflist,
             ["<C-n>"] = require('telescope.actions').cycle_history_next,
             ["<C-p>"] = require('telescope.actions').cycle_history_prev,
+          },
+        },
+      },
+      pickers = {
+        live_grep = {
+          mappings = {
+            i = {
+              ["<C-f>"] = function(prompt_bufnr)
+                local state = require('telescope.actions.state')
+                local current_text = state.get_current_line()
+
+                vim.ui.input({ prompt = "Enter rg parameters (e.g., -t lua, --glob *.rs): " }, function(input)
+                  if input == nil then
+                    return
+                  end
+
+                  local builtin = require('telescope.builtin')
+                  local opts = {
+                    default_text = current_text,
+                    additional_args = function()
+                      if input == "" then
+                        return {}
+                      end
+                      -- Parse the input string into a table
+                      local args = {}
+                      for arg in input:gmatch("[^ ]+") do
+                        table.insert(args, arg)
+                      end
+                      return args
+                    end,
+                  }
+
+                  -- Close current picker and reopen with new args
+                  require('telescope.actions').close(prompt_bufnr)
+                  builtin.live_grep(opts)
+                end)
+              end,
+            },
           },
         },
       },
