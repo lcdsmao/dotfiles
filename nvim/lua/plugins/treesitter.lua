011319b4c9d4e66454dc3458c309e759ec072fcb
Mao <lcdsmao@gmail.com>
2026-01-25 16:35:23 +0900

    feat: treesitter textobj

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -52,4 +52,72 @@ return {
       require('treesitter-context').setup(opts)
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    branch = "main",
+    init = function()
+      -- Disable built-in ftplugin mappings to avoid conflicts
+      vim.g.no_plugin_maps = true
+    end,
+    config = function()
+      local ts_textobjects = require("nvim-treesitter-textobjects")
+      local select = require("nvim-treesitter-textobjects.select")
+      local move = require("nvim-treesitter-textobjects.move")
+
+      -- Setup textobjects
+      ts_textobjects.setup({
+        select = {
+          lookahead = true,
+          include_surrounding_whitespace = false,
+        },
+        move = {
+          set_jumps = true,
+        },
+      })
+
+      -- Text object selections (matches coc: if/af/ic/ac)
+      vim.keymap.set({ "x", "o" }, "if", function()
+        select.select_textobject("@function.inner", "textobjects")
+      end, { desc = "Select inner function" })
+
+      vim.keymap.set({ "x", "o" }, "af", function()
+        select.select_textobject("@function.outer", "textobjects")
+      end, { desc = "Select around function" })
+
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        select.select_textobject("@class.inner", "textobjects")
+      end, { desc = "Select inner class" })
+
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        select.select_textobject("@class.outer", "textobjects")
+      end, { desc = "Select around class" })
+
+      -- Additional useful text objects
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        select.select_textobject("@parameter.inner", "textobjects")
+      end, { desc = "Select inner parameter" })
+
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        select.select_textobject("@parameter.outer", "textobjects")
+      end, { desc = "Select around parameter" })
+
+      -- Movement between functions/classes
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        move.goto_next_start("@function.outer", "textobjects")
+      end, { desc = "Next function start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[f", function()
+        move.goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "Previous function start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "]c", function()
+        move.goto_next_start("@class.outer", "textobjects")
+      end, { desc = "Next class start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[c", function()
+        move.goto_previous_start("@class.outer", "textobjects")
+      end, { desc = "Previous class start" })
+    end,
+  },
 }
