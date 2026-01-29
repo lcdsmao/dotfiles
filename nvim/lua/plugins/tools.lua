5123e0e7cca01e11fd0d64c1349114621e30a0e5
lcdsmao <lcdsmao@gmail.com>
2026-01-29 16:23:58 +0900

    feat: dap float ui picker

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -38,12 +38,59 @@ return {
     },
     config = function()
       require("dapui").setup({})
+      require('telescope').load_extension('dap')
 
       vim.api.nvim_create_user_command("Dap", function()
         require("dapui").toggle()
       end, { bar = true })
 
-      require('telescope').load_extension('dap')
+      vim.api.nvim_create_user_command("DapFloat", function()
+        local pickers = require("telescope.pickers")
+        local finders = require("telescope.finders")
+        local conf = require("telescope.config").values
+        local actions = require("telescope.actions")
+        local action_state = require("telescope.actions.state")
+
+        pickers.new({
+          layout_config = {
+            width = 60,
+            height = 15,
+          },
+        }, {
+          prompt_title = "Dapui Float",
+          finder = finders.new_table({
+            results = {
+              "breakpoints",
+              "console",
+              "repl",
+              "scopes",
+              "stacks",
+              "watches",
+            },
+          }),
+          sorter = conf.generic_sorter({}),
+          attach_mappings = function(prompt_bufnr, map)
+            local function float_element()
+              local selection = action_state.get_selected_entry()
+              if not selection then
+                return
+              end
+
+              actions.close(prompt_bufnr)
+              require("dapui").float_element(selection[1], {
+                close_on_escape = true,
+                position = "center",
+                width = vim.o.columns - 20,
+                height = vim.o.lines - 10,
+              })
+            end
+
+            map("i", "<CR>", float_element)
+            map("n", "<CR>", float_element)
+            return true
+          end,
+        }):find()
+      end, { bar = true })
     end,
   },
 }
