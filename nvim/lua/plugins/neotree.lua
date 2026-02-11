b667c9b6a8717e6b3c6379a44c2ee82c185e1a1e
Mao <lcdsmao@gmail.com>
2026-02-11 13:18:47 +0900

    fix: remove no effect key

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -7,9 +7,9 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
+    lazy = false,                    -- neo-tree will lazily load itself
     keys = {
-      { "<leader>nn", "<cmd>Neotree float reveal<cr>", desc = "Open NeoTree File" },
+      { "<leader>nn", "<cmd>Neotree float reveal<cr>",     desc = "Open NeoTree File" },
       { "<leader>ng", "<cmd>Neotree float git_status<cr>", desc = "Open NeoTree Git" },
     },
     opts = {
@@ -19,12 +19,6 @@ return {
           ["Z"] = "close_all_nodes",
           ["h"] = "close_node",
           ["l"] = "open",
-          ["gs"] = function(state)
-            local reveal_file = vim.fn.expand("#:p")
-            if reveal_file ~= "" then
-              require("neo-tree.command").execute({ action = "focus", reveal_file = reveal_file, dir = vim.fn.getcwd() })
-            end
-          end,
         },
       },
       filesystem = {
