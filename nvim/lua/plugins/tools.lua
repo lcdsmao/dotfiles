b4defacc3426cd0c7db45835d8c9f88eea2819f6
Mao <lcdsmao@gmail.com>
2026-02-11 17:28:27 +0900

    feat: auto sessions

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -93,4 +93,17 @@ return {
       end, { bar = true })
     end,
   },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Downloads", "/" },
+      auto_restore_last_session = true,
+      git_use_branch_name = true,
+      git_auto_restore_on_branch_change = true,
+    },
+  },
 }
