1ca7066740bdb61272e582786a0f52c5f2ec3eda
lcdsmao <lcdsmao@gmail.com>
2026-03-24 17:44:47 +0900

    fix: jdk list versions

--- zshplugins/java.zsh
+++ zshplugins/java.zsh
@@ -1,10 +1,17 @@
 jdk() {
   version=$1
+  if [[ -z $version ]]; then
+    echo "Usage: jdk <version>"
+    echo
+    echo "Available versions:"
+    /usr/libexec/java_home -V
+    echo
+    echo "Current version: $JAVA_HOME"
+    return 1
+  fi
+
   # Big Sur bug: https://developer.apple.com/forums/thread/666681
   unset JAVA_HOME
   export JAVA_HOME=$(/usr/libexec/java_home -v"$version")
   java -version
 }
-
-# Set default JAVA_HOME
-# jdk 21 > /dev/null 2>&1
