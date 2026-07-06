cd6c84f011f75ad6cb9a8fddac61f27188ede72d
lcdsmao <lcdsmao@gmail.com>
2026-07-06 10:46:45 +0900

    feat: skip herdr if no exist

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -2,15 +2,14 @@ return {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
-    init = function()
-      vim.g.tmux_navigator_no_mappings = 1
-    end,
+    init = function() end,
     config = function()
-      vim.g.tmux_navigator_save_on_switch = 2
-      vim.g.tmux_navigator_disable_when_zoomed = 1
-
-      local matches = vim.fn.glob(vim.fn.expand("~/.config/herdr/plugins/github/vim-herdr-navigation-*/editor/nvim.lua"), false, true)
+      local matches =
+        vim.fn.glob(vim.fn.expand("~/.config/herdr/plugins/github/vim-herdr-navigation-*/editor/nvim.lua"), false, true)
       if matches[1] then
+        vim.g.tmux_navigator_no_mappings = 1
+        vim.g.tmux_navigator_save_on_switch = 2
+        vim.g.tmux_navigator_disable_when_zoomed = 1
         dofile(matches[1])
       end
     end,
