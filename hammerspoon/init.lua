e2748068114c3bef8b80dddcbaa4ddd0ba1cce6e
lcdsmao <lcdsmao@gmail.com>
2026-07-07 11:00:46 +0900

    feat: ghostty key window

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,4 +9,20 @@ local open_wezterm = function()
   end
 end
 
-hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "f", open_wezterm)
+local wezterm_hotkey = hs.hotkey.new({ "cmd", "alt", "ctrl", "shift" }, "f", open_wezterm)
+
+local sync_wezterm_hotkey = function()
+  if hs.application.get("Ghostty") ~= nil then
+    wezterm_hotkey:disable()
+  else
+    wezterm_hotkey:enable()
+  end
+end
+
+hs.application.watcher.new(function(app_name, event_type, _)
+  if app_name == "Ghostty" then
+    sync_wezterm_hotkey()
+  end
+end):start()
+
+sync_wezterm_hotkey()
