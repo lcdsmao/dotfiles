bf30da8f2172aa7cb13ed14565371c9c88f297d5
lcdsmao <lcdsmao@gmail.com>
2026-07-27 14:33:53 +0900

    feat: git nvim plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -63,4 +63,17 @@ return {
   {
     "nvim-mini/mini.diff",
   },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "esmuellert/codediff.nvim",
+      "m00qek/baleia.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
+  },
 }
