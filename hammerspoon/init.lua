fd5b40143c5a418be09fe3f9d2ddeb7606d01f6e
lcdsmao <lcdsmao@gmail.com>
2026-08-28 15:18:14 +0900

    feat: use hammerspoon to switch input source instead of karabiner

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,29 @@
+-- Input source switching
+local input_source_ids = {
+  chinese = "com.apple.inputmethod.SCIM.ITABC",
+  japanese = "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese",
+  english = "com.apple.keylayout.ABC",
+}
+
+local switch_input_source = function(source_id)
+  if not hs.keycodes.currentSourceID(source_id) then
+    hs.alert.show("Input source unavailable")
+  end
+end
+
+hs.hotkey.bind({ "ctrl", "shift", "alt" }, "c", function()
+  switch_input_source(input_source_ids.chinese)
+end)
+
+hs.hotkey.bind({ "ctrl", "shift", "alt" }, "j", function()
+  switch_input_source(input_source_ids.japanese)
+end)
+
+hs.hotkey.bind({ "ctrl", "shift", "alt" }, "e", function()
+  switch_input_source(input_source_ids.english)
+end)
+
+-- WezTerm toggle
 local open_wezterm = function()
   local appName = "WezTerm"
   local app = hs.application.get(appName)
@@ -11,6 +37,7 @@ end
 
 local wezterm_hotkey = hs.hotkey.new({ "cmd", "alt", "ctrl", "shift" }, "f", open_wezterm)
 
+-- Disable the WezTerm hotkey while Ghostty is running
 local sync_wezterm_hotkey = function()
   if hs.application.get("Ghostty") ~= nil then
     wezterm_hotkey:disable()
